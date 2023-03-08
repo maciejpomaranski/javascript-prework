dbd8c7830b5208f305bb40252b56330cb29a87ad
Maciej Pomaranski <maciej.pomaranski@gmail.com>
2023-03-08 13:47:49 +0100

    Add playerMove to JavaScript2

--- js/script.js
+++ js/script.js
@@ -10,4 +10,4 @@ function clearMessages(){
 
 let playerMove = `papier`;
 
-printMessage('Zagrałem ' + 'kamień ' + '! Jeśli Twój ruch to + playerMove + to wygrywasz');
+printMessage('Zagrałem ' + 'kamień ' + '! Jeśli Twój ruch to ' + playerMove + 'to wygrywasz');
