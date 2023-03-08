3e12c97a329c4c67051fca4c188302ffd731d163
Maciej Pomaranski <maciej.pomaranski@gmail.com>
2023-03-08 14:50:06 +0100

    Add computerMove code

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
 }
+else if(randomNumber == 2){
+    computerMove = 'papier';
+  }
+  else if(randomNumber == 3){
+    computerMove = 'nożyce';
+  }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
 
-let playerMove = `papier`;
-
-printMessage('Zagrałem ' + 'kamień ' + '! Jeśli Twój ruch to ' + playerMove + 'to wygrywasz');
+printMessage('Mój ruch to: ' + computerMove);
