6b43e9fd96dd7f18bc14ffd43db73a7a636ba4b0
Maciej Pomaranski <maciej.pomaranski@gmail.com>
2023-03-10 16:11:30 +0100

    Add game result2

--- js/script.js
+++ js/script.js
@@ -1,66 +1,21 @@
-let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Wylosowana liczba to: ' + randomNumber);
-
-let computerMove = 'nieznany ruch';
-
-if(randomNumber == 1){
-  computerMove = 'kamień';
-}
-else if(randomNumber == 2){
-    computerMove = 'papier';
-  }
-else(randomNumber == 3){
-computerMove = 'nożyce';
-}
-printMessage('Mój ruch to: ' + computerMove);
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = 'nieznany ruch';
-
-if(playerInput == '1'){
-  playerMove = 'kamień';
-}
-else if(playerInput == '2'){
-    playerMove = 'papier';
-}
-else(playerInput == '3'){
-    playerMove = 'nożyce';
-}
-
-if( computerMove == 'kamień' && playerMove == 'papier'){
-    printMessage('Ty wygrywasz!');
-  }
-else if( computerMove == 'papier' && playerMove == 'papier'){
-printMessage('Remis!');
-}
-else if( computerMove == 'nożyce' && playerMove == 'papier'){
-printMessage('Ty przegrywasz!');
-}
-else if( computerMove == 'nożyce' && playerMove == 'kamiień'){
-printMessage('Ty wygrywasz!');
-} 
-else if( computerMove == 'papier' && playerMove == 'kamiień'){
-printMessage('Ty przegrywasz!');
-}
-else if( computerMove == 'kamień' && playerMove == 'kamiień'){
-printMessage('Remis!');   
-}
-else if( computerMove == 'kamień' && playerMove == 'nożyce'){
-    printMessage('Ty przegrywasz!');   
-}
-else if( computerMove == 'papier' && playerMove == 'nożyce'){
-    printMessage('Ty wygrywasz!');   
-}
-else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
-    printMessage('Remis!');   
-}
-else(playerMove == 'nieznany ruch'){
-    printMessage('nieznany ruch!');
-}
-
-  
-
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    
+    console.log('Wylosowana liczba to: ' + randomNumber);
+    
+    let computerMove = 'nieznany ruch';
+    
+    if(randomNumber == 1){
+      computerMove = 'kamień';
+    }
+    else if(randomNumber == 2){
+        computerMove = 'papier';
+      }
+    else(randomNumber == 3){
+    computerMove = 'nożyce';
+    }
+    
+    printMessage('Mój ruch to: ' + computerMove);
+    
+    
+    l
