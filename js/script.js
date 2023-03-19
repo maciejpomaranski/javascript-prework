41df032083a38ec7cff3e406d2b7ec9071a9fc39
Maciej Pomaranski <maciej.pomaranski@gmail.com>
2023-03-19 13:11:28 +0100

    repair clearMessages

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 function playGame(playerInput){
-    clearMessages
+    clearMessages()
 
     function getMoveName(argMoveId){
             
@@ -18,36 +18,16 @@ function playGame(playerInput){
             console.log('Wylosowana liczba to: ' + randomNumber);
 
             let computerMove = getMoveName(randomNumber);
-        /*
-            if(randomNumber == 1){
-            computerMove = 'kamień';
-            }
-            else if(randomNumber == 2){
-                computerMove = 'papier'
-            }
-            else if(randomNumber == 3){
-                computerMove = 'nożyce'
-            }
-        */  
+       
 
             printMessage('Mój ruch to: ' + computerMove);
 
-            //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+           
 
             console.log('Gracz wpisał: ' + playerInput);
 
             let playerMove = getMoveName(playerInput);
-        /*
-            if(playerInput == '1'){
-                playerMove = 'kamień';
-            }
-            else if(playerInput == 2){
-                playerMove = 'papier'
-            }
-            else if(playerInput == 3){
-                playerMove = 'nożyce'
-            }
-        */
+      
             printMessage('Twój ruch to: ' + playerMove);
             
 
